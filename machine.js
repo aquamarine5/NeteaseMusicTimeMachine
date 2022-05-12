7243dace972e26f3472543f75088ba7d4796033f
aquamarine5 <50990424+aquamarine5@users.noreply.github.com>
2022-05-12 19:27:49 +0800

    use without core

--- machine.js
+++ machine.js
@@ -1,4 +1,4 @@
-const puppeteer = require("puppeteer-core");
+const puppeteer = require("puppeteer");
 const formdata=require("form-data")
 const echarts = require("echarts");
 const axios=require('axios');
